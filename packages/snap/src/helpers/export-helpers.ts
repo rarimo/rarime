3a3a283be99d5dc4dc23b76003f26cac012cc779
electr1xx <aleksandrplotnikov1997@gmail.com>
2023-07-13 17:29:45 +0300

    fix backup structure

--- packages/snap/src/helpers/export-helpers.ts
+++ packages/snap/src/helpers/export-helpers.ts
@@ -3,10 +3,10 @@ import { getItemFromStore } from '../rpc';
 
 export const exportKeysAndCredentials = async () => {
   const credentials = (await getItemFromStore(StorageKeys.credentials)) || [];
-  const identity = (await getItemFromStore(StorageKeys.identity)) || null;
+  const identity = await getItemFromStore(StorageKeys.identity);
 
   return JSON.stringify({
-    identity,
+    privateKey: identity?.privateKeyHex,
     credentials,
   });
 };
