cc3a37ab3389ef145c66030b71932fb32a6c4238
lukachi <lukachisama@gmail.com>
2023-09-13 17:28:44 +0300

    rollback config rarimo core api's

--- packages/snap/src/config.ts
+++ packages/snap/src/config.ts
@@ -4,9 +4,9 @@ export const config = {
   AUTH_BJJ_CREDENTIAL_HASH: 'cca3371a6cb1b715004407e325bd993c',
   ID_TYPE: Uint8Array.from([1, 0]),
 
-  RARIMO_EVM_RPC_URL: 'https://rpc.evm.node1.mainnet-beta.rarimo.com',
-  RARIMO_CORE_URL: 'https://rpc-api.node1.mainnet-beta.rarimo.com',
-  RARIMO_STATE_CONTRACT_ADDRESS: '0x753a8678c85d5fb70A97CFaE37c84CE2fD67EDE8',
+  RARIMO_EVM_RPC_URL: 'https://rpc.evm.mainnet.rarimo.com',
+  RARIMO_CORE_URL: 'https://rpc-api.mainnet.rarimo.com',
+  RARIMO_STATE_CONTRACT_ADDRESS: '0x5ac96945a771d417B155Cb07A3D7E4b8e2F33FdE',
 
   CIRCUIT_AUTH_WASM_URL:
     'https://ipfs.tokend.io/ipfs/ipfs/QmYd41GHrKQLqbk96zHbmHU5rGVcxwmAgBpRqLCGLK7LQu',
