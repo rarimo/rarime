44fc7f6ec661883938d2778133bc9265d656770d
lukachi <lukachisama@gmail.com>
2024-01-30 18:44:21 +0200

    move ceramic npm-scripts into programmatically call-script file

---
+++ packages/snap/ceramic-prepare-vc.mjs
@@ -0,0 +1,64 @@
+import path from 'path';
+import fs from 'fs';
+
+import { fileURLToPath } from 'url';
+import npm from 'npm-commands';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const ceramicUrl = 'http://0.0.0.0:7007';
+const didPK = '';
+
+// Directory path
+const modelsDirPath = './ceramic/models';
+
+const sleep = (ms) =>
+  new Promise(resolve => setTimeout(resolve, ms));
+
+// Reading directory contents
+fs.readdir(modelsDirPath, async (err, files) => {
+  if (err) {
+    console.error('Error reading directory:', err);
+    return;
+  }
+
+  for (const file of files) {
+    if (fs.statSync(path.join(modelsDirPath, file)).isFile()) {
+      console.log('\n\n==========================================================\n\n');
+      console.log('PREPARE VC FOR:', file);
+
+      const modelFile = path.join(
+        __dirname,
+        `./ceramic/models/${file}`,
+      );
+
+      const modelFileName = path.basename(`./ceramic/models/${modelFile}`, '.graphql');
+
+      const compositeOutput = path.join(
+        __dirname,
+        `./ceramic/composites/${modelFileName}.json`,
+      );
+
+      const runtimeOutput = path.join(
+        __dirname,
+        `./ceramic/composites/${modelFileName}-runtime.json`,
+      );
+
+      await npm().arguments({
+        ['ceramic-url']: ceramicUrl,
+        ['output']: compositeOutput,
+        ['did-private-key']: didPK,
+      }).runAsync(`composedb composite:create ${modelFile}`);
+
+      await npm().arguments({
+        ['ceramic-url']: ceramicUrl,
+        ['did-private-key']: didPK,
+      }).runAsync(`composedb composite:deploy ${compositeOutput}`);
+
+      await npm().arguments({
+        ['ceramic-url']: ceramicUrl,
+      }).runAsync(`composedb composite:compile ${compositeOutput} ${runtimeOutput}`);
+    }
+  }
+});
