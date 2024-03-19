0f5353928c70b5045b6b59a9ad99736fa5b48485
Maksym <ardier16@gmail.com>
2024-03-19 15:13:03 +0200

    Remove site package (#42)
    * Remove site package
    
    * Update changelog

--- packages/site/src/utils/button.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { MetamaskSnap } from '@rarimo/rarime-connector';
-import { isLocalSnap } from './snap';
-
-export const shouldDisplayReconnectButton = (installedSnap?: MetamaskSnap) =>
-  installedSnap && isLocalSnap(installedSnap?.snapId);
